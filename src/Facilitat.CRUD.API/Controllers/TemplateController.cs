f7dd21bb1544fcdf5a1fc769b77352258c7621d8
Ícaro Milet <milet.icaro@gmail.com>
2023-09-15 16:48:23 -0300

    feat: infrastructure and domain

---
+++ src/Facilitat.CRUD.API/Controllers/TemplateController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Facilitat.CRUD.Infra;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Facilitat.CRUD.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemplateController : ControllerBase
+    {
+        private readonly TemplateContext _templateContext;
+
+        public TemplateController(TemplateContext templateContext)
+        {
+            _templateContext = templateContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<Template> GetTemplates()
+        {
+            return new List<Template>()
+            {
+                
+            };
+        }
+    }
+}
+
---
+++ src/Facilitat.CRUD.Domain/Aggregates/Template/Entities/Template.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Facilitat.CRUD.Domain.Aggregates.Template.Entities
+{
+    public class Template
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
+
